a4c4a52a1121b925de977c09bfb20f6871b16d95
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-01-04 16:29:30 +0530

    practic js

---
+++ Digital Clock/js/app.js
@@ -0,0 +1,51 @@
+const inter = setInterval(clock, 1000);
+
+function clock() {
+        const monthName = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+        ];
+        const dayName = [
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+        ];
+
+        const today = new Date();
+
+        let hour = today.getHours();
+        let minute = today.getMinutes();
+        let second = today.getSeconds();
+
+        let day = hour < 11 ? "AM" : "PM";
+
+        hour = hour < 10 ? "0" + hour : hour;
+        minute = minute < 10 ? "0" + minute : minute;
+        second = second < 10 ? "0" + second : second;
+        document.querySelector(".full-date").innerHTML =
+            dayName[today.getDay()] +
+            " " +
+            today.getDate() +
+            " " +
+            monthName[today.getMonth()] +
+            " " +
+            today.getFullYear();
+
+        document.querySelector(".hour").innerHTML = hour;
+        document.querySelector(".minute").innerHTML = minute;
+        document.querySelector(".second").innerHTML = second;
+}
