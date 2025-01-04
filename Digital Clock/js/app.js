1cdcd3ab15f275a44c51b3a44247e4437f4806a4
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2025-01-04 17:13:41 +0530

    practic js

--- Digital Clock/js/app.js
+++ Digital Clock/js/app.js
@@ -26,26 +26,23 @@ function clock() {
         ];
 
         const today = new Date();
+        document.querySelector(".full-date").innerHTML =dayName[today.getDay()] + " " + today.getDate() + " " + monthName[today.getMonth()] + " " + today.getFullYear();
 
         let hour = today.getHours();
         let minute = today.getMinutes();
         let second = today.getSeconds();
 
-        let day = hour < 11 ? "AM" : "PM";
+        let day = hour >= 12 ? "PM" : "AM";
+        if(hour >12){
+            hour -= 12;
+        }
 
-        hour = hour < 10 ? "0" + hour : hour;
+        hour = (hour < 10) ? "0" + hour : hour;
         minute = minute < 10 ? "0" + minute : minute;
         second = second < 10 ? "0" + second : second;
-        document.querySelector(".full-date").innerHTML =
-            dayName[today.getDay()] +
-            " " +
-            today.getDate() +
-            " " +
-            monthName[today.getMonth()] +
-            " " +
-            today.getFullYear();
 
         document.querySelector(".hour").innerHTML = hour;
         document.querySelector(".minute").innerHTML = minute;
         document.querySelector(".second").innerHTML = second;
+        document.querySelector(".period").innerHTML = day;
 }
